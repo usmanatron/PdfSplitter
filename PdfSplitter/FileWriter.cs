075eb11441705675ddc22440379b164152ec1383
Usman Iqbal <Usman.Iqbal@softwire.com>
2016-05-13 17:20:12 +0100

    Add terrible hack to temporarily fix a bug with folder creation

--- PdfSplitter/FileWriter.cs
+++ PdfSplitter/FileWriter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace PdfSplitter
 {
@@ -36,6 +30,12 @@ namespace PdfSplitter
 
     public void WriteFile(string fileName, Stream content)
     {
+      //qqUMI Horrible hack to fix bug
+      if (!Directory.Exists(outputFolder))
+      {
+        Directory.CreateDirectory(outputFolder);
+      }
+
       var newFile = File.Create(Path.Combine(outputFolder, fileName));
       content.Seek(0, SeekOrigin.Begin);
       content.CopyTo(newFile);
